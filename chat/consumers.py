4cfe60942dfea945d43ae46da5f4d88e8d277805
Natalia Teplynina <nt@mnt9.com>
2022-08-22 09:58:05 +0300

    add frontend

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,18 +10,19 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+        await self.accept()
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'tester_message',
-                'tester': 'chatty sock'
+                'tester': 'chatty sock',
             }
         )
 
     async def tester_message(self, event):
         tester = event['tester']
         await self.send(text_data=json.dumps({
-            'tester': tester
+            'tester': tester,
         }))
 
     async def disconnect(self, close_code):
