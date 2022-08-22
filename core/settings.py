610ef4401956f621d36d144574c16448d4a04294
Natalia Teplynina <nt@mnt9.com>
2022-08-22 12:07:29 +0300

    sort out templates

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'channels',
+    'core',
     'users',
     'chat',
 ]
@@ -122,6 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
