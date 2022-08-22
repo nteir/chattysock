a2938e7e4e61314a1da684385fa3f86d5f797935
Natalia Teplynina <nt@mnt9.com>
2022-08-22 11:04:22 +0300

    handle chatroom messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,22 +11,26 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
         await self.accept()
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'chatty sock',
-            }
-        )
-
-    async def tester_message(self, event):
-        tester = event['tester']
-        await self.send(text_data=json.dumps({
-            'tester': tester,
-        }))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.room_group_name
         )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message,
+        }))
