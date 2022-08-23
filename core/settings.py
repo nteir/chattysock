5ed3f55405903f99857e201e98de9218adb3b8f0
Natalia Teplynina <nt@mnt9.com>
2022-08-23 10:15:43 +0300

    add reddis settings for localhost

--- core/settings.py
+++ core/settings.py
@@ -139,6 +139,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)]
+        }
     }
 }
